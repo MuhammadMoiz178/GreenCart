27ff7326335229c39b83676fee2f488dd50f9e73
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-01-17 21:06:01 +0500

    add Navbar and Mainbanner components; implement routing in App component

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,13 +7,14 @@ export const AppContextProvider = ({children}) => {
     const navigate = useNavigate()
     const [user,setUser] = useState(null)
     const [isSeller,setIsSeller] = useState(false)
+    const [showUserLogin,setShowUserLogin] = useState(false)
 
-    const value = {navigate,user,setUser,isSeller,setIsSeller}
+    const value = {navigate,setUser,user,isSeller,setIsSeller,showUserLogin,setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
 
 export const useAppContext = () => {
-    useContext(AppContext)
+   return useContext(AppContext)
 }
