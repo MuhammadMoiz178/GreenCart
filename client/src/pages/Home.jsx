035442eb3120fa1d2055d19ce3ae37a00cff24f4
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-01-17 22:48:17 +0500

    add Categories component and integrate it into Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Mainbanner from '../components/Mainbanner'
+import Categories from '../components/Categories'
 
 function Home() {
   return (
     <div className='mt-10'>
         <Mainbanner/>
+        <Categories/>
     </div>
   )
 }
