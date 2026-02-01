f8411c62e1e63d8380da3c7bddf460115d7602d2
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-02-01 23:46:34 +0500

    refactor Navbar and AllProducts components to integrate search functionality and improve product filtering

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -6,15 +6,17 @@ function AllProducts() {
     const {products,searchQuery} = useAppContext()
     const [filteredProducts,setFilteredProducts] = useState([])
     
-    useEffect(()=>{
-        if(searchQuery.length>0) {
-            setFilteredProducts(products.filter((product)=>(
-                product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
-            )))
-        } else {
-            setFilteredProducts(products)
-        }
-    },[products,searchQuery])
+    useEffect(() => {
+    if (searchQuery.length > 0) {
+      setFilteredProducts(
+        products.filter((product) =>
+          product.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredProducts(products);
+    }
+  }, [products, searchQuery]);
   return (
     <div className='mt-16 flex flex-col'>
         <div className='flex flex-col items-end w-max'>
