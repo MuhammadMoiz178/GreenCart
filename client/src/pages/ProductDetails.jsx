9825bb45309164b53f2f29084ebc6defe1ff4752
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-02-04 23:51:10 +0500

    add related products section to ProductDetails page and update styles

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext';
 import { Link, useParams } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import ProductCard from '../components/ProductCard';
 
 function ProductDetails() {
   const {products,navigate,currency,addToCart} = useAppContext()
@@ -13,13 +14,13 @@ function ProductDetails() {
     const product = products.find((item)=>item._id===id)
 
     useEffect(()=>{ 
-        if(product.length>0) {
+        if(products.length>0) {
             let productCopy = products.slice() 
         // Shallow copy
             productCopy = productCopy.filter((item)=>product.category===item.category)
             setRelatedProducts(productCopy.slice(0,5)) 
         }
-    },[product])
+    },[products])
 
     useEffect(()=>{
         setThumbnail(product?.image[0]?product.image[0]:null)
@@ -30,7 +31,7 @@ function ProductDetails() {
                 <Link to={"/"}>Home</Link> /
                 <Link to={"/products"}> Products</Link> /
                 <Link to={`/products/${product.category.toLowerCase()}`}> {product.category}</Link> /
-                <span className="text-indigo-500"> {product.name}</span>
+                <span className="text-primary"> {product.name}</span>
             </p>
 
             <div className="flex flex-col md:flex-row gap-16 mt-4">
@@ -77,12 +78,35 @@ function ProductDetails() {
                             Add to Cart
                         </button>
                         <button onClick={()=>{addToCart(product._id);navigate('/cart')}}
-                        className="w-full py-3.5 cursor-pointer font-medium bg-indigo-500 text-white hover:bg-indigo-600 transition" >
+                        className="w-full py-3.5 cursor-pointer font-medium bg-primary-dull text-white hover:bg-indigo-600 transition" >
                             Buy now
                         </button>
                     </div>
                 </div>
             </div>
+         {/*------------related Products------------*/}
+        <div className="flex flex-col items-center mt-20">
+          <div className="flex flex-col items-center w-max">
+            <p className="text-3xl font-medium">Related Products</p>
+            <div className="w-20 h-0.5 bg-[var(--color-primary)] rounded-full mt-2"></div>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+            {relatedProducts
+              .filter((product) => product.inStock)
+              .map((product, index) => (
+                <ProductCard key={index} product={product} />
+              ))}
+          </div>
+          <button
+            onClick={() => {
+              navigate("/products");
+              scrollTo(0.0);
+            }}
+            className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-[var(--color-primary)] hover:bg-[var(--color-primary-dull)]/10 transition"
+          >
+            See more
+          </button>
+        </div>
         </div>
     );  
 }
