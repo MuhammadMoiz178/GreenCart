ee508cbda24f83c1cf241d444cf145100e51ed36
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-02-05 23:38:38 +0500

    add getCartCount and getCartAmount functions to AppContext and update Navbar to display cart item count

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
 function Navbar() {
     const [open, setOpen] = React.useState(false)
-    const {user,setUser,setShowUserLogin,navigate,searchQuery,setSearchQuery} = useAppContext()
+    const {user,setUser,setShowUserLogin,navigate,searchQuery,setSearchQuery,getCartCount} = useAppContext()
     
 
     const logout = async () => {
@@ -39,7 +39,8 @@ function Navbar() {
                 <div onClick={()=>navigate('/cart')} 
                 className="relative cursor-pointer">
                     <img src={assets.cart_icon} alt="cart" className='w-6 opacity-80'/>
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+                    {getCartCount()}</button>
                 </div>
 
                {!user? (<button onClick={()=>setShowUserLogin(true)} 
@@ -58,12 +59,19 @@ function Navbar() {
                     </div>
                 )}
             </div>
-
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+            
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={()=>navigate('/cart')} 
+                className="relative cursor-pointer">
+                    <img src={assets.cart_icon} alt="cart" className='w-6 opacity-80'/>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+                    {getCartCount()}</button>
+                </div>
+                 <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
                 {/* Menu Icon SVG */}
                <img src={assets.menu_icon} alt="menu" />
             </button>
-
+            </div>
             {/* Mobile Menu */}
             { open && (
 
