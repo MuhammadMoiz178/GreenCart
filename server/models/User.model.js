ff1cfe7683532cb7ed7bc36b42fd51c12f6123e0
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-02-22 13:56:10 +0500

    feat(auth): implement user registration with JWT and password hashing

---
+++ server/models/User.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    cartItems:{
+        type:Object,
+        default:{},
+    }
+},{minimize:false})
+
+const User = mongoose.models.user || mongoose.model('user',UserSchema)
+
+export default User
