9e0d87cc48df1a8ef912da2a149e367951437860
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-02-23 19:59:13 +0500

    feat(product): add product model and controller with CRUD endpoints

---
+++ server/routes/seller.route.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { isSellerAuth, sellerLogin, sellerLogout } from '../controllers/seller.controller.js'
+import authSeller from '../middlewares/authseller.middleware.js'
+
+const sellerRouter = express.Router()
+
+sellerRouter.post('/login',sellerLogin)
+sellerRouter.get('/is-auth',authSeller,isSellerAuth)
+sellerRouter.get('/logout',sellerLogout)
+
+
+export default sellerRouter
