a012f0045494e51bd12b3e8d491adb0227cc14a9
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-03-02 14:07:09 +0500

    fix(orders): add optional chaining for address and amount properties

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -44,14 +44,14 @@ function Orders() {
                     </div>
 
                     <div className="text-sm md:text-base text-black/60">
-                        <p className='text-black/80'>{order.address.firstName} {order.address.lastName}</p>
-                        <p>{order.address.street}, {order.address.city} </p>
-                        <p> {order.address.state}, {order.address.zipcode}, {order.address.country}</p>
+                        <p className='text-black/80'>{order.address?.firstName} {order.address?.lastName}</p>
+                        <p>{order.address?.street}, {order.address?.city} </p>
+                        <p> {order.address?.state}, {order.address?.zipcode}, {order.address?.country}</p>
                         <p></p>
-                        <p>{order.address.phone}</p>
+                        <p>{order.address?.phone}</p>
                     </div>
 
-                    <p className="font-medium text-lg my-auto">{currency}{order.amount}</p>
+                    <p className="font-medium text-lg my-auto">{currency}{order?.amount}</p>
 
                     <div className="flex flex-col text-sm md:text-base text-black/60">
                         <p>Method: {order.paymentType}</p>
